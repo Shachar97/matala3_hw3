e7ca6c49b711e5101aea4dd10ec58dce5f7b2be8
Shachar97 <shachar.ketz@gmail.com>
2022-12-28 22:11:05 +0200

    add debag for insertion function

--- isort.c
+++ isort.c
@@ -1,7 +1,7 @@
 #include "isort.h"
 #include <stdio.h>
 // #define DEBUGER_SWAP
-#define DEBUGER_SHIFT
+// #define DEBUGER_SHIFT
 #define DEBUGER_INSERT
 
 void swap(int* arr, int a, int b){//O(1)
@@ -20,64 +20,72 @@ void swap(int* arr, int a, int b){//O(1)
 }
 
 void shift_element(int* arr, int i){//O(i)
-#ifdef DEBUGER_SHIFT
-    printf("~~~~in shift_element:~~~~\n\tthe array from the pointer to i: [");
-    int counter = 1;
-    int* adr = arr;
-    while(counter<=i)
-    {
-        printf("%d ",*(adr));
-        adr = arr+counter;
-        counter++;
-    }
-    printf("]");
-    
-#endif
-
-    int j = 0;
-
     #ifdef DEBUGER_SHIFT
+        printf("~~~~in shift_element:~~~~\n\tthe array from the pointer to i: [");
+        int counter = 1;
+        int* adr = arr;
+        while(counter<=i){
+            printf("%d ",*(adr));
+            adr = arr+counter;
+            counter++;
+        }
+        printf("]");
         printf("\n\tthe array after shift element:\t[");
     #endif
+
+    int j = 0;
 
     while (j<i)
     {
         swap(arr ,j,i);
+
         #ifdef DEBUGER_SHIFT
             printf("%d ", *(arr+j));
         #endif
+
         j++;
     }
-    
+
     #ifdef DEBUGER_SHIFT
         printf("]\n");
         printf("~~~~~~~~~~~~~~~~\n");
     #endif
+
     return;
 }
 
 void insertion_sort(int* arr , int len){
     
-    if (len==1) {return;}
+    if (len==1) {return;}//array is orgenized
     
-    for (size_t i = 0; i < len-1; i++){
+    #ifdef DEBUGER_INSERT
+        printf("~~~in insertion_sort function:~~~~\n");
+        printf("\tthe array befor sort: [");
+        for (size_t i = 0; i < len; i++){
+            printf("%d ",*(arr+i));
+        }
+        printf("]\n");
+    #endif
+    for (size_t i = 1; i < len; i++){
+        int j = 0;
+        while ( (j < i) && ( *(arr+j) < *(arr+i) )){
 
-       for (size_t j = i+1; i < len; j++){
+            j++;
+        }
 
-            int temp = 0;
-            while ( (temp < j) && ( *(arr+temp) < *(arr+j) )){
+        if ( *(arr+j) > *(arr+i) ){
 
-                temp++;
+            int shifts = i-j;
+            shift_element(arr+j,shifts);
+        }
+        #ifdef DEBUGER_INSERT
+            printf("\tthe array after sort: [");
+            for (size_t i = 0; i < len; i++){
+                printf("%d ",*(arr+i));
             }
-
-            if ( *(arr+temp) > *(arr+j) ){
-
-                int shifts = j-temp;
-                shift_element(arr+temp,shifts);
-            }
-            
-       }
-       
+            printf("]\n");
+            printf("~~~~~~~~~~~\n");
+        #endif
     }
     return;    
 }
